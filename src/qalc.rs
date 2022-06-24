feec650e408f6df9847cec6fc951083770ca5688
Maxime Bouillot <maxime.bouillot@withings.com>
2022-06-24 20:18:55 +0200

    No more panics on rpn_qalc.

--- src/qalc.rs
+++ src/qalc.rs
@@ -3,14 +3,14 @@ use std::process::Command;
 /// From a computation given as input, returns the error and the output of
 /// qalc in RPN mode.
 pub fn rpn_qalc(computation: &str) -> String {
-    let cmd = Command::new("qalc")
+    match Command::new("qalc")
         .arg("-set")
         .arg("rpn on")
         .arg(computation)
-        .output()
-        .expect("Unable to start the `qalc` command");
-    let err = std::str::from_utf8(&cmd.stderr).unwrap().to_string();
-    let out = std::str::from_utf8(&cmd.stdout).unwrap().to_string();
-    err + &out
+        .output() {
+            Ok(cmd) => std::str::from_utf8(&cmd.stderr).unwrap().to_string() +
+                std::str::from_utf8(&cmd.stdout).unwrap(),
+            Err(_) => "Error, qalc not found on the host system.".to_string(),
+    }
 }
 
