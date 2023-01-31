c395880e0311a1e272963bef8132ddff6bc516ec
Maxime Bouillot <maxime.bouillot@withings.com>
2023-01-31 10:31:56 +0100

    Clean up tz filtering.
    This ensures that even if the content of zoneinfo
    is not the same as on my system, no strange
    behavior can be observed. The worst case possible
    is some time-zones not being found.

--- src/tz.rs
+++ src/tz.rs
@@ -4,13 +4,13 @@ const TZ_PATH: &str = "/usr/share/zoneinfo/";
 
 /// Try to fix timezones like PDT or PST into ones that work with the rest of
 /// the system like PST8PDT
-fn quick_patch_tz(tz: &str) -> Option<&str> {
+fn quick_patch_tz(tz: &str) -> &str {
     match tz {
-        "PST" | "PDT" => Some("PST8PDT"),
-        "CST" | "CDT" => Some("CST6CDT"),
-        "EST" | "EDT" => Some("EST5EDT"),
-        "MT" => Some("America/Phoenix"),
-        _ => None,
+        "PST" | "PDT" => "PST8PDT",
+        "CST" | "CDT" => "CST6CDT",
+        "EST" | "EDT" => "EST5EDT",
+        "MT" => "America/Phoenix",
+        _ => tz,
     }
 }
 
@@ -24,25 +24,15 @@ fn is_tz_valid(tz: &str) -> bool {
         .arg(tz_path)
         .output()
         .expect("find command failed to start");  
-    match output.status.success() {
-        true => true,
-        false => match quick_patch_tz(tz) {
-            Some(x) => is_tz_valid(x),
-            None => false,
-        },
-    }
+    output.status.success()
 }
 
 /// Returns the time at the timezone given in argument.
 /// It is assumed that the given timezone is valid.
 fn time_at_valid_tz(tz: &str) -> String {
-    let true_tz = match quick_patch_tz(tz) {
-        Some(x) => x,
-        None => tz,
-    };
     let output = Command::new("date")
         .arg("+%H:%M")
-        .env("TZ", true_tz)
+        .env("TZ", tz)
         .output()
         .expect("date command failed to start")
         .stdout;
@@ -52,8 +42,9 @@ fn time_at_valid_tz(tz: &str) -> String {
 /// If the timezone given as argument is valid, returns the time there as a
 /// string. Otherwise, returns None.
 pub fn time_at_tz(tz: &str) -> Option<String> {
-    match is_tz_valid(tz) {
-        true  => Some(time_at_valid_tz(tz)),
+    let filtered_tz = quick_patch_tz(tz);
+    match is_tz_valid(filtered_tz) {
+        true  => Some(time_at_valid_tz(filtered_tz)),
         false => None,
     }
 }
