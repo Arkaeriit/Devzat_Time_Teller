3cb45a3a8d9df97beb4d73af63be63889ef0c8a9
Maxime Bouillot <maxbouillot@gmail.com>
2023-03-17 10:40:38 +0000

    Add a quirky substitution from 🥕 to ^.

--- src/qalc.rs
+++ src/qalc.rs
@@ -3,10 +3,11 @@ use std::process::Command;
 /// From a computation given as input, returns the error and the output of
 /// qalc in RPN mode.
 pub fn rpn_qalc(computation: &str) -> String {
+    let replaced = computation.replace("🥕", "^");
     match Command::new("qalc")
         .arg("-set")
         .arg("rpn on")
-        .arg(computation)
+        .arg(&replaced)
         .output() {
             Ok(cmd) => std::str::from_utf8(&cmd.stderr).unwrap().to_string() +
                 std::str::from_utf8(&cmd.stdout).unwrap(),
